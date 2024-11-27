03cfc9f92f8040faf9b2179d37cbf7e1f5c0ad14
Oserebameh Augustine, Beckley <beckaustine@outlook.com>
2024-11-27 23:35:55 +0100

    Helper function to track camera position.

--- src/components/World.jsx
+++ src/components/World.jsx
@@ -1,20 +1,30 @@
-import { Canvas } from '@react-three/fiber';
+import { Canvas, useThree, useFrame} from '@react-three/fiber';
 import { OrbitControls, RandomizedLight, Stage } from '@react-three/drei';
 import { useLoader } from '@react-three/fiber'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+import { useEffect } from 'react';
 
 
 function Model() {
     const gltf = useLoader(GLTFLoader, '/lamborghini_terzo_millennio.glb')
     return <primitive object={gltf.scene} />
   }
-  
+function CameraLogger() {
+const { camera } = useThree();
 
+useFrame(() => {
+    console.log(camera.position);
+});
+
+return null;
+}
+  
 function World(){
     return (
       <div className='h-screen w-screen'>
           <Canvas>
-            <Stage adjustCamera intensity={0.1} shadows="contact" environment="city">
+            <CameraLogger />
+            <Stage intensity={0.1} shadows="contact" environment="city">
                 <Model />
             </Stage>
             <OrbitControls />
