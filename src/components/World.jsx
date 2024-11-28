2cf5418833d926f902b393bb0aa948a7b3c9cd18
Oserebameh Augustine, Beckley <beckaustine@outlook.com>
2024-11-28 09:08:35 +0100

    Animate camera as intro effect.

--- src/components/World.jsx
+++ src/components/World.jsx
@@ -2,30 +2,26 @@ import { Canvas, useThree, useFrame} from '@react-three/fiber';
 import { OrbitControls, RandomizedLight, Stage } from '@react-three/drei';
 import { useLoader } from '@react-three/fiber'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
-import { useEffect } from 'react';
+import { animate } from 'motion';
 
 
 function Model() {
     const gltf = useLoader(GLTFLoader, '/lamborghini_terzo_millennio.glb')
     return <primitive object={gltf.scene} />
   }
-function CameraLogger() {
-const { camera } = useThree();
-
-useFrame(() => {
-    console.log(camera.position);
-});
-
-return null;
+function AnimateCamera() {
+    const { camera } = useThree();
+    animate(camera.position, { x: 2, y: 0, z: 3 }, { duration: 4, ease: 'easeInOut', delay:0.75 });
+    return null;
 }
   
 function World(){
     return (
       <div className='h-screen w-screen'>
           <Canvas
-            camera={{ position: [0, 2, -1.5], fov: 40 }}
+            camera={{ position: [1, 1, -3], fov: 30 }}
           >
-            <CameraLogger />
+            <AnimateCamera />
             <Stage adjustCamera={false} intensity={0.1} shadows="contact" environment="city">
                 <Model />
             </Stage>
