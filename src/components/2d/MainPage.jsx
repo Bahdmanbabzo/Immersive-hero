225d5e105f9943a4974e8e5cd650791b9e25fd34
Oserebameh Augustine, Beckley <beckaustine@outlook.com>
2024-11-28 18:04:08 +0100

    Center child elements.

--- src/components/2d/MainPage.jsx
+++ src/components/2d/MainPage.jsx
@@ -7,12 +7,12 @@ function MainPage() {
         animate('p', { y: ["100%", "0%"] }, { duration: 1, delay: 0 });
     },[])
   return (
-    <main ref={scope} className='text-3xl font-bold text-red-500 z-40 absolute top-0'>
-        <div className="overflow-hidden">
-            <p>LAMBORGHINI TERZO MILLENNIO</p>
-        </div>
-        <div className="overflow-hidden">
-            <p>Concept Car</p>
+    <main ref={scope} className=' text-black z-40 relative top-0 h-screen w-screen grid place-items-center'>
+        <div className="overflow-hidden absolute bottom-0 m-5 flex flex-col items-center">
+            <div className="overflow-hidden font-light text-lg">
+                <p>Concept Car</p>
+            </div>
+            <p className="text-5xl font-semibold">LAMBORGHINI TERZO MILLENNIO</p>
         </div>
     </main>
   )
