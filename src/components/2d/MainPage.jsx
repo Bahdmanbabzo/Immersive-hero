b7ad7107edc99a7b378b7eb3bac49294f6b2a949
Oserebameh Augustine, Beckley <beckaustine@outlook.com>
2024-11-28 17:49:21 +0100

    Intro animation for the main page

--- src/components/2d/MainPage.jsx
+++ src/components/2d/MainPage.jsx
@@ -1,8 +1,28 @@
+import { motion, useAnimate } from "motion/react"
+import { useEffect } from "react";
+
 function MainPage() {
+    const [scope, animate] = useAnimate();
+    useEffect(() => {
+        animate('p', { y: 0 }, { duration: 1, delay: 0 });
+    },[])
   return (
-    <div className='text-3xl font-bold text-red-500 z-40 absolute'>
-      This is the main page with tailwind 
-    </div>
+    <main ref={scope} className='text-3xl font-bold text-red-500 z-40 absolute top-0'>
+        <div className="overflow-hidden">
+            <motion.p
+                initial={{ y: "100%"}}
+            >
+                LAMBORGHINI TERZO MILLENNIO
+            </motion.p>
+        </div>
+        <div className="overflow-hidden">
+            <motion.p
+                initial={{ y: "100%"}}
+            >
+                Concept Car
+            </motion.p>
+        </div>
+    </main>
   )
 }
 
