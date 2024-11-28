5143232c7ed01706d916c79e38dccd65ea93a576
Oserebameh Augustine, Beckley <beckaustine@outlook.com>
2024-11-28 17:52:58 +0100

    Set intro and exit animations with animate.

--- src/components/2d/MainPage.jsx
+++ src/components/2d/MainPage.jsx
@@ -4,23 +4,15 @@ import { useEffect } from "react";
 function MainPage() {
     const [scope, animate] = useAnimate();
     useEffect(() => {
-        animate('p', { y: 0 }, { duration: 1, delay: 0 });
+        animate('p', { y: ["100%", "0%"] }, { duration: 1, delay: 0 });
     },[])
   return (
     <main ref={scope} className='text-3xl font-bold text-red-500 z-40 absolute top-0'>
         <div className="overflow-hidden">
-            <motion.p
-                initial={{ y: "100%"}}
-            >
-                LAMBORGHINI TERZO MILLENNIO
-            </motion.p>
+            <p>LAMBORGHINI TERZO MILLENNIO</p>
         </div>
         <div className="overflow-hidden">
-            <motion.p
-                initial={{ y: "100%"}}
-            >
-                Concept Car
-            </motion.p>
+            <p>Concept Car</p>
         </div>
     </main>
   )
